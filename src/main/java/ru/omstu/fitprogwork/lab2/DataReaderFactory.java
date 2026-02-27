6f160736541188e1540da012af1510b2ab1bc903
Бочанов Сергей <2222.e9j@gmail.com>
2026-02-27 18:21:17 +0600

    feat: changed factory code

--- src/main/java/ru/omstu/fitprogwork/lab2/DataReaderFactory.java
+++ src/main/java/ru/omstu/fitprogwork/lab2/DataReaderFactory.java
@@ -1,12 +1,22 @@
 package ru.omstu.fitprogwork.lab2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DataReaderFactory {
+    static final Map<String, DataReader> readers = new HashMap<>();
+
+    static {
+        readers.put("json", new JsonDataReader());
+        readers.put("xml", new XmlDataReader());
+    }
+
     public static DataReader getReader(String fileName) {
-        if (fileName.endsWith(".json")) {
-            return new JsonDataReader();
-        } else if (fileName.endsWith(".xml")) {
-            return new XmlDataReader();
+        String extension = fileName.substring(fileName.lastIndexOf(".") + 1).toLowerCase();
+        DataReader reader = readers.get(extension);
+        if (reader == null) {
+            throw new IllegalArgumentException("Неизвестный формат файла");
         }
-        throw new IllegalArgumentException("Неизвестный формат файла");
+        return reader;
     }
 }
