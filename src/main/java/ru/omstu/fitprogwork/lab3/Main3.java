2e23605f626c03108c8207b251b2fef2414bef83
Бочанов Сергей <2222.e9j@gmail.com>
2026-04-25 00:21:40 +0600

    feat: changed cacheable usage to map cache service

--- src/main/java/ru/omstu/fitprogwork/lab3/Main3.java
+++ src/main/java/ru/omstu/fitprogwork/lab3/Main3.java
@@ -2,10 +2,8 @@ package ru.omstu.fitprogwork.lab3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class Main3 {
     static void main(String[] args) { SpringApplication.run(Main3.class, args); }
 }
